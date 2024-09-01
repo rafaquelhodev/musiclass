93e02fd0867d18fee016456f540b559ccb264c9c
Rafael <rafael.quelho@gmail.com>
2024-09-01 18:47:39 -0300

    serving static page

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/static"
 )
 
 func main() {
@@ -11,10 +12,12 @@ func main() {
 	app := fiber.New()
 
 	// Define a route for the GET method on the root path '/'
-	app.Get("/", func(c fiber.Ctx) error {
-		// Send a string response to the client
-		return c.SendString("Hello, World 👋!")
-	})
+	// app.Get("/", func(c fiber.Ctx) error {
+	// 	// Send a string response to the client
+	// 	return c.SendString("Hello, World 👋!")
+	// })
+
+	app.Get("/", static.New("./client/music-class/dist"))
 
 	// Start the server on port 3000
 	log.Fatal(app.Listen(":3000"))
